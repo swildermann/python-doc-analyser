74bcbc9075fa0b0bd814da9242ba68fa10144386
Sven Widlermann <bitbucket@wildermannonline.de>
2014-04-13 17:14:56 +0200

    changed the readme script
    Former-commit-id: 484819784213fbe46ecb17abd29649133b6c8e92

--- README/readme-skript.sh
+++ README/readme-skript.sh
@@ -2,9 +2,10 @@
 #Python 3.3 muss bereits instaliert sein
 # libsqlite3-dev muss vor der Installation von python3 installiert sein
 sudo apt-get install libsqlite3-dev
-
-
-
+# sudo apt-get install python-pi
+#pip install django
+#pip install django-twitter-bootstrap==3.1.1
+#pip install south
 
 #username for "south": root
 #mail for south: pythondev@wildermannonline.de

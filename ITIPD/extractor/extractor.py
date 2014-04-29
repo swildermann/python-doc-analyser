19b249923100d8df677c7831ad51c504f3972136
Sven Wildermann <bitbucket@wildermannonline.de>
2014-04-29 22:36:29 +0200

    introduced IDE PyCharm - some small changes
    Former-commit-id: 5323e9df30b76c5028f0a117d2ae953233a4d528

--- ITIPD/extractor/extractor.py
+++ ITIPD/extractor/extractor.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup, Tag
 import re
 import psycopg2
 
+
 def bs_preprocess(html):
     """remove distracting whitespaces and newline characters
     (c) mail-group of beautifulsoup4"""
@@ -11,13 +12,15 @@ def bs_preprocess(html):
                                      # this preserves newline delimiters
     html = re.sub('[\s]+<', '<', html) # remove whitespaces before opening tags
     html = re.sub('>[\s]+', '>', html) # remove whitespaces after closing tags
-    return html 
+    return html
+
 
 def summarize_placeholders(parent,string):
     """ removes multiple placeholders if they are next to each other"""
     for elem in parent.find_all("p"):
         while isinstance(elem.next_sibling, Tag) and elem.next_sibling.name== 'p' and elem.text==string and elem.next_sibling.text==string:
             elem.next_sibling.extract()
+
 
 def find_parents(childs):
     """ finds the parent of each BeautifulSoup-Element given in a list (childs).
@@ -34,7 +37,8 @@ def find_parents(childs):
                 break 
         if not b_found:
             parents.append(child.findParent())
-    return parents 
+    return parents
+
 
 def offset(elements):
     """get the offsets of the given element"""
@@ -50,8 +54,9 @@ def offset(elements):
         startoffset.append(length)
         endoffset.append(length+len(str(elem)))  
         length=0
-    return (startoffset, endoffset)      
-        
+    return (startoffset, endoffset)
+
+
 def file_to_soup(path):
     '''open a file and make a soup out of that'''
     g = open(path,"r")
@@ -60,6 +65,7 @@ def file_to_soup(path):
     return soup
 
 soup=file_to_soup("/home/sven/Bachelorarbeit/python-doc-extractor-for-cado/ITIPD/extractor/index.html")
+
 
 def grab_elements(soup,elem,attr1,attr2):
     """grabs the different elemens with the given attributes out of a soup"""
@@ -72,9 +78,6 @@ classmethods = grab_elements(soup,"dl","class","classmethod")
 staticmethods = grab_elements(soup,"dl","class","staticmethod")
 sections = grab_elements(soup,"div","class","sections")
 
-###grab different elements 
-#dl_elems = soup.find_all(['dl'], attrs={'class': ['class', 'method','function','describe', 'classmethod', 'staticmethod']}) 
-#sections = soup.find_all(['div'], attrs = {'class':'section'}) 
 
 ###define placeholders
 method_string = '[method(s)-removed-here]'
@@ -107,7 +110,7 @@ for parent in methods + functions + describtions + classmethods + staticmethods 
 
 results = methods + functions + describtions + classmethods + staticmethods + sections
 ###output file
-f = open('test.html','w')    #needs to exist
+f = open('test.html', 'w')  # needs to exist
 f.truncate
 ###create the output
 print(results,file=f)
@@ -115,16 +118,17 @@ f.close()
     
 
 #########STORE SOMETHING INTO THE DATABASE#############
-i = 1
 conn = psycopg2.connect("dbname=mydb user=sven")
 cur = conn.cursor()
 parents = find_parents(results)
+i = 1
 for elem in results:
-    i=i+1
+    i = i + 1
     fname = "None"
     if isinstance(elem, Tag): 
          strng = elem.prettify()
-         cur.execute("""INSERT INTO extractor_documentationunit  VALUES (%s, %s, %s, %s, %s, %s);""", (i,strng,0,fname,0,0))
+         cur.execute('INSERT INTO extractor_documentationunit  VALUES (%s,  %s,  %s,  %s,  %s, %s);',
+                     (i, strng, 0, fname, 0, 0))
 conn.commit()
 cur.close()
 conn.close()

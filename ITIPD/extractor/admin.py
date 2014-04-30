3ce43e67230f152da2591568e163435a84058fe0
Sven Wildermann <bitbucket@wildermannonline.de>
2014-04-30 10:08:29 +0200

    try to get rid of spaghetti code
    Former-commit-id: 015b2b98aac30b80735e2074d91c3ea6057dc8d0

--- ITIPD/extractor/admin.py
+++ ITIPD/extractor/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from extractor.models import DocumentationUnit, KnowledgeType
 
+
 class KnowledgeTypesAdmin(admin.ModelAdmin):
     list_display = ['name'
-
 ]
+
 
 class DocumentationUnitAdmin(admin.ModelAdmin):
     list_display = ['id', 'filename']
@@ -12,5 +13,3 @@ class DocumentationUnitAdmin(admin.ModelAdmin):
 
 admin.site.register(DocumentationUnit,DocumentationUnitAdmin)
 admin.site.register(KnowledgeType,KnowledgeTypesAdmin)
-# Register your models here.
-# Register your models here.
--- ITIPD/extractor/extractor.py
+++ ITIPD/extractor/extractor.py
@@ -4,9 +4,6 @@ import psycopg2
 from os import listdir
 from os.path import isfile, join
 
-mypath = "/home/sven/Bachelorarbeit/python-doc-extractor-for-cado/ITIPD/extractor/python-3.4.0-docs-html/"
-
-
 def get_list_of_filepath(mypath):
     pathlist = []
     onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
@@ -72,14 +69,11 @@ def offset(elements):
 
 
 def file_to_soup(path):
-    '''open a file and make a soup out of that'''
+    """open a file and make a soup out of that"""
     g = open(path, "r")
     data = g.read()
     soup = BeautifulSoup(bs_preprocess(data))
     return soup
-
-
-soup = file_to_soup("/home/sven/Bachelorarbeit/python-doc-extractor-for-cado/ITIPD/extractor/index.html")
 
 
 def grab_elements(soup, elem, attr1, attr2):
@@ -87,50 +81,99 @@ def grab_elements(soup, elem, attr1, attr2):
     return soup.find_all([elem], attrs={attr1: [attr2]})
 
 
-methods = grab_elements(soup, "dl", "class", "method")
-functions = grab_elements(soup, "dl", "class", "function")
-describtions = grab_elements(soup, "dl", "class", "describe")
-classmethods = grab_elements(soup, "dl", "class", "classmethod")
-staticmethods = grab_elements(soup, "dl", "class", "staticmethod")
-sections = grab_elements(soup, "div", "class", "sections")
+def grab_all(soup):
+    methods = grab_elements(soup, "dl", "class", "method")
+    functions = grab_elements(soup, "dl", "class", "function")
+    describtions = grab_elements(soup, "dl", "class", "describe")
+    classmethods = grab_elements(soup, "dl", "class", "classmethod")
+    staticmethods = grab_elements(soup, "dl", "class", "staticmethod")
+    sections = grab_elements(soup, "div", "class", "sections")
+    classes = grab_elements(soup, "dl", "class", "class")
+    attributes = grab_elements(soup, "dl", "class", "attribute")
+    datas = grab_elements(soup, "dl", "class", "data")
+
+    #    return methods + functions + describtions + classmethods + staticmethods + sections + classes + attributes + datas
+
+    #def placeholding(allunits):
+    ###define placeholders
+    method_string = '[method(s) removed here]'
+    function_string = '[function(s) removed here]'
+    describe_string = '[descriptions removed here]'
+    classmethod_string = '[classmethod removed here]'
+    staticmethod_string = '[staticmethod removed here]'
+    class_string = '[class(es) removed here]'
+    section_string = '[section removed here]'
+    attribute_string = '[attribute removed here]'
+    datas_string = '[data removed here]'
+
+    for parent in methods + functions + describtions + classmethods + staticmethods + sections + classes + attributes + datas:
+        ### set placeholders to duplicated elements ###
+        for elem in parent.find_all('dl', {'class': 'method'}):
+            tag = Tag(name='p')
+            tag.string = method_string
+            elem.replace_with(tag)
+        for elem in parent.find_all('dl', {'class': 'function'}):
+            tag = Tag(name='p')
+            tag.string = function_string
+            elem.replace_with(tag)
+        for elem in parent.find_all('dl', {'class': 'describe'}):
+            tag = Tag(name='p')
+            tag.string = describe_string
+            elem.replace_with(tag)
+        for elem in parent.find_all('dl', {'class': 'classmethod'}):
+            tag = Tag(name='p')
+            tag.string = classmethod_string
+            elem.replace_with(tag)
+        for elem in parent.find_all('dl', {'class': 'staticmethod'}):
+            tag = Tag(name='p')
+            tag.string = staticmethod_string
+            elem.replace_with(tag)
+        for elem in parent.find_all('dl', {'class': 'class'}):
+            tag = Tag(name='p')
+            tag.string = class_string
+            elem.replace_with(tag)
+        for elem in parent.find_all('div', {'class': 'section'}):
+            tag = Tag(name='p')
+            tag.string = section_string
+            elem.replace_with(tag)
+        for elem in parent.find_all('dl', attrs={'class': 'attribute'}):
+            tag = Tag(name='p')
+            tag.string = attribute_string
+            elem.replace_with(tag)
+        for elem in parent.find_all('dl', attrs={'class': 'data'}):
+            tag = Tag(name='p')
+            tag.string = datas_string
+            elem.replace_with(tag)
+
+        ### summarize placeholders ###
+        summarize_placeholders(parent, method_string)
+        summarize_placeholders(parent, function_string)
+        summarize_placeholders(parent, describe_string)
+        summarize_placeholders(parent, classmethod_string)
+        summarize_placeholders(parent, staticmethod_string)
+        summarize_placeholders(parent, class_string)
+        summarize_placeholders(parent, section_string)
+        summarize_placeholders(parent, attribute_string)
+        summarize_placeholders(parent, datas_string)
+        return methods + functions + describtions + classmethods + staticmethods + sections + classes + attributes + datas
 
 
-###define placeholders
-method_string = '[method(s)-removed-here]'
-class_string = '[class(es) removed here]'
-section_string = '[section removed here]'
-attribute_string = '[attribute / data ignored here]'
-for parent in methods + functions + describtions + classmethods + staticmethods + sections:
-    ### set placeholders to duplicated elements ###
-    for elem in parent.find_all('dl',
-                                attrs={'class': ['method', 'function', 'describe', 'classmethod', 'staticmethod']}):
-        tag = Tag(name='p')
-        tag.string = method_string
-        elem.replace_with(tag)
-    for elem in parent.find_all('dl', {'class': 'class'}):
-        tag = Tag(name='p')
-        tag.string = class_string
-        elem.replace_with(tag)
-    for elem in parent.find_all('div', {'class': 'section'}):
-        tag = Tag(name='p')
-        tag.string = section_string
-        elem.replace_with(tag)
-    for elem in parent.find_all('dl', attrs={'class': ['attribute', 'data']}):
-        tag = Tag(name='p')
-        tag.string = attribute_string
-        elem.replace_with(tag)
-    ### summarize placeholders ###
-    summarize_placeholders(parent, method_string)
-    summarize_placeholders(parent, class_string)
-    summarize_placeholders(parent, section_string)
-    summarize_placeholders(parent, attribute_string)
+if __name__ == "__main__":
+    mypath = "/home/sven/Bachelorarbeit/python-doc-extractor-for-cado/ITIPD/extractor/python-3.4.0-docs-html/"
+    files = get_list_of_filepath(mypath)
 
-results = methods + functions + describtions + classmethods + staticmethods + sections
-###output file
-f = open('test.html', 'w')  # needs to exist
-###create the output
-print(results, file=f)
-f.close()
+    soup = file_to_soup("/home/sven/Bachelorarbeit/python-doc-extractor-for-cado/ITIPD/extractor/index.html")
+    units = grab_all(soup)
+    #    parents = find_parents(units)
+    #    new_units = placeholding(units)
+
+    ###output file
+    f = open('test.html', 'w')  # needs to exist
+    ###create the output
+    print(units, file=f)
+    print("**********", file=f)
+    #    print(parents, file=f)
+    f.close()
 
 
 #########STORE SOMETHING INTO THE DATABASE#############

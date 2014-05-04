775bd6f1a226591d4a06c581d0c16ae230ba5c68
Sven Wildermann <bitbucket@wildermannonline.de>
2014-05-04 18:22:51 +0200

    added new model: MappingUnitToUser
    Former-commit-id: 3f74d3bc83d78f1124b7b6e03e16feec153b22f7

--- ITIPD/extractor/models.py
+++ ITIPD/extractor/models.py
@@ -22,3 +22,9 @@ class MarkedUnit(models.Model):
     #documentation_unit = models.IntegerField(default=0)
     knowledge_type = models.IntegerField(default=0)
     html_text = models.TextField()
+
+
+class MappingUnitToUser(models.Model):
+    user = models.ForeignKey('auth.User', null=True, blank=True)
+    documentation_unit = models.ForeignKey(DocumentationUnit)
+    already_marked = models.BooleanField(default=False)

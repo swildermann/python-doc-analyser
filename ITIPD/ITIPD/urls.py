300bd7ca376f22a38fb500742de5aa58b98191f5
Sven Wildermann <bitbucket@wildermannonline.de>
2014-05-17 00:05:54 +0200

    changed urls again
    Former-commit-id: 89b97bbcf8d59b2cc55d07ffe2f58962f8f647dc

--- ITIPD/ITIPD/urls.py
+++ ITIPD/ITIPD/urls.py
@@ -9,5 +9,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^pydocs/', include('extractor.urls', namespace="documentationunit")),
+    url(r'^pydoc/', include('extractor.urls', namespace="documentationunit")),
 )

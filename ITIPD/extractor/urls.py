6d138280e2c76c69b80b96887531491f776dd732
Sven Wildermann <bitbucket@wildermannonline.de>
2014-05-16 14:44:43 +0200

    added link to coding handbook, added base url
    Former-commit-id: ab2842b4690115727226bdaa28d5c0ace7ec3add

--- ITIPD/extractor/urls.py
+++ ITIPD/extractor/urls.py
@@ -13,6 +13,7 @@ urlpatterns = patterns('',
       url(r'^myunits/$', views.show_next_unit, name='show_next'),
       url(r'^markedunits/$', views.marked_units, name='marked_units'),
       url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/extractor/login'}),
-      url(r'^randomunit/$', views.random_mapping, name = 'random_mapping')
+      url(r'^randomunit/$', views.random_mapping, name = 'random_mapping'),
+      url(r'^', views.show_next_unit, name='show_next')
 
 )

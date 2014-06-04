d608cb2a5a1fb140c1ab56d9c872772d5b08b92e
Sven Wildermann <bitbucket@wildermannonline.de>
2014-06-04 12:47:02 +0200

    did a fake migration. Needs to be done manually with sql on the production server
    Former-commit-id: 05851e6c06efdb9ddd66a5e86ca2cb3b2ce87c80

---
+++ ITIPD/extractor/migrations/0005_auto__add_field_mappingunittouser_unmarked_chars.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'MappingUnitToUser.unmarked_chars'
+        db.add_column('extractor_mappingunittouser', 'unmarked_chars',
+                      self.gf('django.db.models.fields.IntegerField')(default=0),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'MappingUnitToUser.unmarked_chars'
+        db.delete_column('extractor_mappingunittouser', 'unmarked_chars')
+
+
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '80', 'unique': 'True'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'unique_together': "(('content_type', 'codename'),)", 'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'related_name': "'user_set'", 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'related_name': "'user_set'", 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'max_length': '30', 'unique': 'True'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'unique_together': "(('app_label', 'model'),)", 'db_table': "'django_content_type'", 'ordering': "('name',)", 'object_name': 'ContentType'},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'extractor.accesslog': {
+            'Meta': {'object_name': 'AccessLog'},
+            'documentation_unit': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['extractor.DocumentationUnit']"}),
+            'filename': ('django.db.models.fields.CharField', [], {'max_length': '100', 'default': "''"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'timestamp': ('django.db.models.fields.DateTimeField', [], {'default': '0'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']", 'blank': 'True', 'null': 'True'})
+        },
+        'extractor.documentationunit': {
+            'Meta': {'object_name': 'DocumentationUnit'},
+            'file_text': ('django.db.models.fields.TextField', [], {'default': "''"}),
+            'filename': ('django.db.models.fields.CharField', [], {'max_length': '500', 'default': "'None'"}),
+            'html_text': ('django.db.models.fields.TextField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'length': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'offset': ('django.db.models.fields.IntegerField', [], {'default': '-1'}),
+            'parent_text': ('django.db.models.fields.TextField', [], {'default': "''"}),
+            'plaintext': ('django.db.models.fields.TextField', [], {}),
+            'type': ('django.db.models.fields.CharField', [], {'max_length': '100', 'default': "''"})
+        },
+        'extractor.knowledgetype': {
+            'Meta': {'object_name': 'KnowledgeType'},
+            'description': ('django.db.models.fields.TextField', [], {'default': "''"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        },
+        'extractor.mappingunittouser': {
+            'Meta': {'object_name': 'MappingUnitToUser'},
+            'already_marked': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'documentation_unit': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['extractor.DocumentationUnit']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'last_change': ('django.db.models.fields.DateTimeField', [], {'default': '0'}),
+            'unmarked_chars': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']", 'blank': 'True', 'null': 'True'})
+        },
+        'extractor.markedunit': {
+            'Meta': {'object_name': 'MarkedUnit'},
+            'char_range': ('django.db.models.fields.TextField', [], {'max_length': '500', 'default': "''"}),
+            'documentation_unit': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['extractor.DocumentationUnit']"}),
+            'html_text': ('django.db.models.fields.TextField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'knowledge_type': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'range': ('django.db.models.fields.TextField', [], {'max_length': '500', 'default': "''"}),
+            'timestamp': ('django.db.models.fields.DateTimeField', [], {'default': '0'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']", 'blank': 'True', 'null': 'True'})
+        }
+    }
+
+    complete_apps = ['extractor']

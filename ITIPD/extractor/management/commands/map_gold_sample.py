8bee158a4f24032cfd985737f7ea71dad434a36d
Sven Wildermann <bitbucket@wildermannonline.de>
2014-06-04 13:18:44 +0200

    initial commit for issue #48
    Former-commit-id: 0e8b9c63163588a7711038b1e14e130a4fa79d0f

--- ITIPD/extractor/management/commands/map_gold_sample.py
+++ ITIPD/extractor/management/commands/map_gold_sample.py
@@ -1,1 +1,4 @@
-__author__ = 'sven'
+from django.core.management.base import BaseCommand, CommandError
+from extractor.models import DocumentationUnit, MappingUnitToUser
+import random
+from django.contrib.auth.models import User

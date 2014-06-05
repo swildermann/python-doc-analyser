f3b0a1047817547808993c694b3bc02fa1820f92
Sven Wildermann <bitbucket@wildermannonline.de>
2014-06-05 22:40:42 +0200

    fixes #51 - admin command is called calculate_unmarked_chars
    Former-commit-id: 2a881207f161b6b2907dc2b5873aa1a0f27a2963

---
+++ ITIPD/extractor/management/commands/calculate_unmarked_chars.py
@@ -0,0 +1,20 @@
+from django.core.management.base import BaseCommand, CommandError
+from extractor.models import DocumentationUnit, MappingUnitToUser, MarkedUnit
+from extractor.views import how_much_is_unmarked
+import random
+from django.contrib.auth.models import User
+
+
+class Command(BaseCommand):
+    help = 'maps the gold sample'
+
+    def handle(self, *args, **options):
+        self.stdout.write("***START***")
+
+        all_units = MappingUnitToUser.objects.all()
+        for each in all_units:
+            unmarked_results = how_much_is_unmarked(each.user,each.documentation_unit.id)
+            self.stdout.write(str(each.documentation_unit.id))
+            each.unmarked_chars = unmarked_results[0]
+            each.unmarked_percent = unmarked_results[1]
+            each.save()

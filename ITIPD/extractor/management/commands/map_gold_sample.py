170373af9944be86a0e12932c930a6ed89bf8267
Sven Wildermann <bitbucket@wildermannonline.de>
2014-06-08 17:15:02 +0200

    fixes #48 - wrote a command for mapping the gold sampline (python manage.py map_gold_sample)
    Former-commit-id: 065d1537b0f270ed9192948a87c2e5ea2a942009

--- ITIPD/extractor/management/commands/map_gold_sample.py
+++ ITIPD/extractor/management/commands/map_gold_sample.py
@@ -2,59 +2,65 @@ from django.core.management.base import BaseCommand, CommandError
 from extractor.models import DocumentationUnit, MappingUnitToUser
 import random
 from django.contrib.auth.models import User
+from django.db.models import Q
 
 
 class Command(BaseCommand):
     help = 'maps the gold sample'
-    def create_mapping(self,object_list):
-        self.stdout.write("***START***")
+    
+    def create_mapping(self,object_list,debug,number):
+        random.shuffle(object_list)
+        object_list = object_list[:number]
+        self.stdout.write("counting:"+str(len(object_list)))
+        self.stdout.write(str(object_list))
         all_validaters = User.objects.filter(groups__name='validaters')
         for object in object_list:
+            unit=DocumentationUnit.objects.get(id=object)
             for validater in all_validaters:
-                mapUnitToUser = MappingUnitToUser.objects.create(
-                    user=validater,
-                    documentation_unit=object.documentation_unit,
-                    already_marked=False,
-                    last_change =  "1900-01-01 00:00:00",
-                    unmarked_chars = len(object.documentation_unit.plaintext),
-                    unmarked_percent = 100
-                )
-        self.stdout.write("***END***")
+                if debug==False:
+                    mapUnitToUser = MappingUnitToUser.objects.create(
+                        user=validater,
+                        documentation_unit=unit,
+                        already_marked=False,
+                        last_change =  "1900-01-01 00:00:00",
+                        unmarked_chars = len(unit.plaintext),
+                        unmarked_percent = 100
+                    )
 
     def handle(self, *args, **options):
-        # TODO: Only get distinct documentation_units
-        #TODO: It maps to many units (+78 more than it should - why!?!) 
-        ClassOrException = (MappingUnitToUser.objects.filter(documentation_unit__type='class',
-                                                             user__groups__name="Students")) | \
-                           MappingUnitToUser.objects.filter(documentation_unit__type='exception',
-                                                            user__groups__name="Students") \
-                            .order_by('?')[:17]
-        AttributeOrData = (MappingUnitToUser.objects.filter(documentation_unit__type='attribute',
-                                                             user__groups__name="Students") | \
-                           MappingUnitToUser.objects.filter(documentation_unit__type='error',
-                                                            user__groups__name="Students")) \
-                            .order_by('?')[:30]
-        MethodOrFunction = (MappingUnitToUser.objects.filter(documentation_unit__type='method',
-                                                             user__groups__name="Students") | \
-                           MappingUnitToUser.objects.filter(documentation_unit__type='staticmethod',
-                                                            user__groups__name="Students") |\
-                           MappingUnitToUser.objects.filter(documentation_unit__type='classmethod',
-                                                            user__groups__name="Students") |\
-                           MappingUnitToUser.objects.filter(documentation_unit__type='function',
-                                                            user__groups__name="Students"))  \
-                           .order_by('?')[:88]
-        Describe = (MappingUnitToUser.objects.filter(documentation_unit__type='describe',
-                                                     user__groups__name="Students")) \
-                            .order_by('?')[:1]
-        Section = (MappingUnitToUser.objects.filter(documentation_unit__type='section',
-                                                    user__groups__name="Students")) \
-                            .order_by('?')[:32]
-        Command.create_mapping(self, ClassOrException)
-        Command.create_mapping(self, AttributeOrData)
-        Command.create_mapping(self, MethodOrFunction)
-        Command.create_mapping(self, Describe)
-        Command.create_mapping(self, Section)
+        self.stdout.write("***START***\n")
+        ClassOrException = list(MappingUnitToUser.objects.filter(Q(user__groups__name="Students"),
+                             Q(documentation_unit__type="class") | Q(documentation_unit__type="exception")) \
+                            .distinct("documentation_unit") \
+                            .values_list("documentation_unit__pk", flat=True))
 
+        AttributeOrData = list(MappingUnitToUser.objects.filter(Q(user__groups__name="Students"),
+                            Q(documentation_unit__type="attribute") | Q(documentation_unit__type="data")) \
+                            .distinct("documentation_unit") \
+                            .values_list("documentation_unit__pk", flat=True))
 
+        MethodOrFunction = list(MappingUnitToUser.objects.filter(Q(user__groups__name="Students"),
+                            Q(documentation_unit__type="method") | Q(documentation_unit__type="staticmethod") | \
+                            Q(documentation_unit__type="classmethod") | Q(documentation_unit__type="function")) \
+                            .distinct("documentation_unit") \
+                            .values_list("documentation_unit__pk", flat=True))
+
+        Describe = list(MappingUnitToUser.objects.filter(Q(user__groups__name="Students"),
+                    Q(documentation_unit__type="describe")) \
+                    .distinct("documentation_unit") \
+                    .values_list("documentation_unit__pk", flat=True))
+
+        Section = list(MappingUnitToUser.objects.filter(Q(user__groups__name="Students"),
+                    Q(documentation_unit__type="section")) \
+                    .distinct("documentation_unit") \
+                    .values_list("documentation_unit__pk", flat=True))
+
+        Command.create_mapping(self, ClassOrException,False,17)
+        Command.create_mapping(self, AttributeOrData, False, 30)
+        Command.create_mapping(self, MethodOrFunction, False, 88)
+        Command.create_mapping(self, Describe, False, 1)
+        Command.create_mapping(self, Section, False, 32)
+
+        self.stdout.write("***END***")
 
 

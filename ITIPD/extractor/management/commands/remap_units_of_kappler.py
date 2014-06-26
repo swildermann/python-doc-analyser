3d81d8e754a8ce1e80ed1276d9a5d70dd1ff4f59
Sven Wildermann <uni@wildermannonline.de>
2014-06-26 12:04:18 +0200

    working on the additional mapping of 28 units
    Former-commit-id: 77de1a0d83710c04ed0339f9be4da27bc5414837

--- ITIPD/extractor/management/commands/remap_units_of_kappler.py
+++ ITIPD/extractor/management/commands/remap_units_of_kappler.py
@@ -12,19 +12,25 @@ class Command(BaseCommand):
     help = 'maps a sample to the 7 students with their id'
 
     def get_units(self):
+
+        how_full = []
+
         kapplers_units = MappingUnitToUser.objects.filter(user__username="robert", already_marked="False")\
             .order_by("-documentation_unit__id").values_list("documentation_unit__id", flat=True)[:221]
 
         extra_28 = User.objects.filter(groups__name="extra_28").values_list("username",flat=True)
+        extra_28_as_list=[]
+        for each in extra_28:
+            extra_28_as_list.append(each)
+            how_full.append({each:0})
+
         for each in kapplers_units:
             already_mapped_to = MappingUnitToUser.objects.filter(documentation_unit__id=each)\
                 .values_list("user__username", flat=True)
             self.stdout.write("Already mapped to:")
             self.stdout.write(str(already_mapped_to))
 
-            new_students=[]
-            for each in extra_28:
-                new_students.append(each)
+            new_students = copy.deepcopy(extra_28_as_list)
             for each in already_mapped_to:
                 if each in new_students:
                     new_students.remove(each)
@@ -35,6 +41,7 @@ class Command(BaseCommand):
             self.stdout.write("new students:")
             self.stdout.write(str(new_students))
 
+        self.stdout.write(str(how_full))
 
 
     def handle(self, *args, **options):

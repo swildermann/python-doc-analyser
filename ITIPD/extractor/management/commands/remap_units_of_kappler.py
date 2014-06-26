b3349cedd37d142974090d36ce3bc3c1a427c6cd
Sven Wildermann <uni@wildermannonline.de>
2014-06-26 11:10:58 +0200

    working on the additional mapping of 28 units
    Former-commit-id: 2dec5460160f5c81d925e889bbb2744bfe26d9b5

---
+++ ITIPD/extractor/management/commands/remap_units_of_kappler.py
@@ -0,0 +1,37 @@
+from django.core.management.base import BaseCommand, CommandError
+from extractor.models import DocumentationUnit, MappingUnitToUser
+import random, copy
+from django.contrib.auth.models import User
+"""
+map half of the units of Robert Kappler to other students
+
+"""
+
+
+class Command(BaseCommand):
+    help = 'maps a sample to the 7 students with their id'
+
+    def get_units(self):
+        kapplers_units = MappingUnitToUser.objects.filter(user__username="robert", already_marked="False")\
+            .order_by("-documentation_unit__id").values_list("documentation_unit__id", flat=True)[:221]
+
+        extra_28 = User.objects.filter(groups__name="extra_28")
+
+        for each in kapplers_units:
+            already_mapped_to = MappingUnitToUser.objects.filter(documentation_unit__id=each)\
+                .values_list("user", flat=True)
+            self.stdout.write("Already mapped to:")
+            self.stdout.write(str(already_mapped_to))
+
+            new_students = copy.deepcopy(extra_28)
+            for each in already_mapped_to:
+                if each in new_students:
+                    new_students.remove(each)
+            self.stdout.write("new students:")
+            self.stdout.write(str(new_students))
+
+
+
+    def handle(self, *args, **options):
+
+        Command.get_units(self)

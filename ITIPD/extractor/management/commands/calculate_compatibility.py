a5453d95bf43dd4d57f315acc20ebd8240464c43
swildermann <git@wildermannonline.de>
2014-07-09 14:08:49 +0200

    added a command to calculate the compatibility for >all< units of the students
    Former-commit-id: b2e477f83789c0120b2f41f1386b393a8915971d

---
+++ ITIPD/extractor/management/commands/calculate_compatibility.py
@@ -0,0 +1,24 @@
+from django.core.management.base import BaseCommand, CommandError
+from extractor.models import *
+from extractor.views import calculate_compatiblity
+
+
+
+class Command(BaseCommand):
+    help = 'calculates the compatibility of 2 units'
+
+    def handle(self, *args, **options):
+        self.stdout.write("***START***")
+
+        all_units = MappingUnitToUser.objects.all()
+        status_array = []
+        for each in all_units:
+            status = calculate_compatiblity(each.user,each.documentation_unit.id)
+            if isinstance(status,bool):
+                status_array.append(status)
+            else:
+                status_array.append(True)
+            self.stdout.write(str(status))
+
+        self.stdout.write(str(status_array))
+        self.stdout.write(str(len([x for x in status_array  if x == True])))

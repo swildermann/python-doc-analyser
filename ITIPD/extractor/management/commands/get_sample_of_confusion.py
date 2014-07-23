2e90ce3afcf1ccf4c823b91f276ff20894c3dbf1
swildermann <git@wildermannonline.de>
2014-07-23 15:39:16 +0200

    adding another command, still working on it, just commit for testing purposes
    Former-commit-id: fa340f91189a4d0032df212614dd2ead5a1a51a1

--- ITIPD/extractor/management/commands/get_sample_of_confusion.py
+++ ITIPD/extractor/management/commands/get_sample_of_confusion.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand, CommandError
 from extractor.models import *
 from django.db.models import Q
+from django.contrib.auth.models import User
 
 
 
@@ -14,26 +15,24 @@ class Command(BaseCommand):
             raise CommandError('get_sample_of_confusion takes exactly 2 arguments')
         first_type = KnowledgeType.objects.get(pk=args[0])
         second_type = KnowledgeType.objects.get(pk=args[1])
-        new = Confusions.objects.filter(Q(atype_id=first_type, btype_id=second_type) \
-            | Q(atype_id=second_type, btype_id=first_type)).count()
+        confusion = Confusions.objects.filter(Q(atype_id=first_type, btype_id=second_type) \
+            | Q(atype_id=second_type, btype_id=first_type))
+        for unit in confusion:
+            DocUnit1 = DocumentationUnit.objects.get(pk=MarkedUnit.objects.get(pk=unit.idofa).documentation_unit_id)
+            DocUnit2= DocumentationUnit.objects.get(pk=MarkedUnit.objects.get(pk=unit.idofb).documentation_unit_id)
+            if DocUnit1!=DocUnit2:
+                raise CommandError('Something went wrong. Trying to compare different documentation units')
+            User1 = User.objects.get(pk=MarkedUnit.objects.get(pk=unit.idofa).user_id).username
+            User2 = User.objects.get(pk=MarkedUnit.objects.get(pk=unit.idofb).user_id).username
+            if User1==User2:
+                raise CommandError('can not compare the units of the same user')
 
-        self.stdout.write(str(new))
 
-        #, btype_id=args[1]) | (btype_id=args[0], atype_id=args[1]))
+            self.stdout.write(User1, User2, DocUnit1)
+
+            self.stdout.write('test')
 
 
 
 
-        self.stdout.write('Jop')
-
-        # self.stdout.write("***START***")
-        #
-        # all_units = MappingUnitToUser.objects.all()
-        # status_array = []
-        # for each in all_units:
-        #     status = calculate_agreement(each.user,each.documentation_unit.id)
-        #     status_array.append(status)
-        #     self.stdout.write(str(status))
-        #
-        # self.stdout.write(str(status_array))
-        # self.stdout.write(str(len([x for x in status_array  if x == True])))
+        self.stdout.write(str(len(confusion)))

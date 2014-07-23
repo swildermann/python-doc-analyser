768040c37b60b2021ebb23df7ccc240c23d4ca6b
swildermann <git@wildermannonline.de>
2014-07-23 16:46:33 +0200

    still working on the command
    Former-commit-id: d9b9b3fabcb9fabdb74a4fea25aa667d515f3751

--- ITIPD/extractor/management/commands/get_sample_of_confusion.py
+++ ITIPD/extractor/management/commands/get_sample_of_confusion.py
@@ -30,7 +30,7 @@ class Command(BaseCommand):
                 raise CommandError('can not compare the units of the same user')
 
 
-            self.stdout.write(User1.username, User2.username, DocUnit1.id)
+            self.stdout.write(User1.username, User2.username, str(DocUnit1.id))
 
             self.stdout.write('test')
 

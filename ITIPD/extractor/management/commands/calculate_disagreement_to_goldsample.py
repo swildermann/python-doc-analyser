3e67af1bf3362b1e8518240e6bacfbf24c619874
Sven Wildermann <uni@wildermannonline.de>
2014-07-31 17:55:51 +0200

    try to find the reason for strange results
    Former-commit-id: 7c6b51a827d70fc45baa1fdbbef7f425a8d711c2

--- ITIPD/extractor/management/commands/calculate_disagreement_to_goldsample.py
+++ ITIPD/extractor/management/commands/calculate_disagreement_to_goldsample.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 from extractor.models import *
 from extractor.views import merge_markings
+from django.contrib.auth.models import User
 
 
 class Command(BaseCommand):

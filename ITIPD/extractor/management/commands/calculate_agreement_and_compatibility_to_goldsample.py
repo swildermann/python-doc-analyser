87a24997b9fc74ac1bbeac458f680076da23f79e
Sven Wildermann <uni@wildermannonline.de>
2014-08-01 15:22:44 +0200

    added another command for rating the coders, small fix
    Former-commit-id: 52d177346b19a261b5ed73c8ee8494dd058a28fb

--- ITIPD/extractor/management/commands/calculate_agreement_and_compatibility_to_goldsample.py
+++ ITIPD/extractor/management/commands/calculate_agreement_and_compatibility_to_goldsample.py
@@ -61,13 +61,14 @@ class Command(BaseCommand):
                         length_of_all_false += get_length_of_marking(key, my_results, results_to_compare)
                         falses+= 1
 
-                how_much_true_in_total+=how_much_is_true
+
                 total_length = length_of_all_trues + length_of_all_false
                 how_much_is_true = round((length_of_all_trues/total_length)*100,5)
                 how_much_is_false = round((length_of_all_false/total_length)*100,5)
                 total = trues+falses
                 in_percent = round(trues/total * 100,5)
-
+                how_much_true_in_total+=how_much_is_true
+                
             self.stdout.write("username:" +str(user.username))
             if counter!=0:
                 how_much_true_in_total=how_much_true_in_total/counter

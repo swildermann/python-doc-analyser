62d2bb8071c360d978e6fa009ee89541b7f38ebe
swildermann <git@wildermannonline.de>
2014-08-01 19:04:50 +0200

    added another command
    Former-commit-id: 1e46192c8192182616183c404a3dc4a26126eda0

--- ITIPD/extractor/management/commands/merge_sample.py
+++ ITIPD/extractor/management/commands/merge_sample.py
@@ -1,4 +1,4 @@
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
 from extractor.merge_goldsample import *
 
 
@@ -8,12 +8,45 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         self.stdout.write("***START***")
 
-        all_units = MappingUnitToUser.objects.filter(user__groups__name='validators')\
-            .distinct('documentation_unit')\
-            .values_list('documentation_unit__pk', flat=True)
+        all_units = MappingUnitToUser.objects.filter(user__groups__name='Students')\
+            .distinct('documentation_unit')
         status_array = []
-        for each in all_units:
-            status = calculate_best_goldsample(each)
-            status_array.append(status)
+        for unit in all_units:
+            first_mapped_id = MappingUnitToUser.objects.get(pk=unit.id)
+            all_other_mapped= MappingUnitToUser.objects.exclude(user=first_mapped_id.user)\
+                .filter(user__groups__name="Students",
+                        documentation_unit=unit.documentation_unit,
+                        already_marked=True)
+            if len(all_other_mapped)>1:
+                raise CommandError("!Found to many mappings to this unit: "+str(unit.documentation_unit.id)+
+                                   ". Please have a close look to the code!! "+str(len(all_other_mapped)))
+            if len(all_other_mapped)==0:
+                self.stdout.write("Found no additional mappings to this unit: "+str(unit.documentation_unit.id)+
+                                  " Please have a close look to the code! "+str(len(all_other_mapped)))
+                continue
+            second_mapped_id = all_other_mapped[0]
 
-        self.stdout.write(str(status_array))
+            self.stdout.write(".",ending='')
+            first_markings = MarkedUnit.objects.filter(user=first_mapped_id.user,
+                                                       documentation_unit=first_mapped_id.documentation_unit)\
+                                                       .values('id', 'char_range','knowledge_type')
+            second_markings = MarkedUnit.objects.filter(user=second_mapped_id.user,
+                                                        documentation_unit=second_mapped_id.documentation_unit)\
+                                                        .values('id', 'char_range','knowledge_type')
+            if len(first_markings)==len(second_markings)==0:
+                # TODO: save a empty unit to the endresult (and count them maybe?)
+                continue
+            if len(first_markings)==0 or len(second_markings)==0:
+                #TODO save that with markings or without? Save without and count them additional?
+                continue
+
+            first_results = merge_markings(first_markings)
+            second_results = merge_markings(second_markings)
+
+
+            #Markierungen vergleichen
+                #Bei Konfusion: Pluspunkt vergeben
+                #Bei Gleichheit: Gutachter mit mehr Pluspunkten bevorzugen (Markierung übernehmen) (ODER auf "besseren" Gutachter zurückgreifen (Markierung übernehmen))
+                #Weder Konfusion noch Gleichheit: Erst Pluspunkte, dann bessere Gutachter
+
+

c1e338411d346cfe5934861fbac45ba1c717fabd
Sven Wildermann <uni@wildermannonline.de>
2014-08-04 15:27:29 +0200

    calculate_correlation added
    Former-commit-id: 8102ff3747107a43744d7befcefc5977d77ef1aa

---
+++ ITIPD/extractor/management/commands/calculate_correlation.py
@@ -0,0 +1,24 @@
+from django.core.management.base import BaseCommand
+from extractor.models import *
+
+
+
+class Command(BaseCommand):
+    help = 'calculates correlation'
+
+    def handle(self, *args, **options):
+        self.stdout.write("***START***")
+
+        for knowledge in range(1,13):
+            results = [0,0,0,0,0,0,0,0,0,0,0,0]
+            self.stdout.write("current: "+str(knowledge))
+            all_units_with_this_marking = MarkedUnit.objects.filter(user__username="results",knowledge_type=knowledge)\
+                .values_list('documentation_unit__id',flat=True)
+            for unit in all_units_with_this_marking:
+                other_markings = MarkedUnit.objects.exclude(knowledge_type=knowledge).filter(user__username="results",
+                    documentation_unit__id=unit)\
+                    .distinct("knowledge_type").values_list("knowledge_type",flat=True)
+                for marking in other_markings:
+                    results[marking-1]+=1
+            self.stdout.write(str(results))
+

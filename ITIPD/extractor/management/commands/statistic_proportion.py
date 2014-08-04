b0ef6fbaa020fbcdcf61616fab6265b60d3c2034
Sven Wildermann <uni@wildermannonline.de>
2014-08-04 12:07:43 +0200

    added another command for statistic
    Former-commit-id: 6d96e84125f29941556b1d0309cbd420c16be2fb

---
+++ ITIPD/extractor/management/commands/statistic_proportion.py
@@ -0,0 +1,59 @@
+from django.core.management.base import BaseCommand
+from extractor.models import *
+
+
+
+class Command(BaseCommand):
+    help = 'calculates the agreement of 2 units'
+
+    def handle(self, *args, **options):
+        self.stdout.write("***START***")
+
+        all_types = DocumentationUnit.objects.all().distinct("type").values_list("type",flat=True)
+        total = (0,0)
+        for type in all_types:
+            self.stdout.write("*************")
+            methods = (0,0)
+            fields = (0,0)
+            modules = (0,0)
+            classes = (0,0)
+            describe = (0,0)
+            for knowledge in range(1,13):
+                self.stdout.write("knowledge-type:"+str(knowledge))
+                count_markings = MarkedUnit.objects.filter(user__groups__name="Students",documentation_unit__type=type,
+                                          knowledge_type=knowledge).distinct('documentation_unit').count()
+                count_units = MappingUnitToUser.objects.filter(user__groups__name="Students",
+                                                               documentation_unit__type=type)\
+                    .distinct('documentation_unit').count()
+                self.stdout.write("markings: "+str(count_markings))
+                self.stdout.write("units: "+str(count_units))
+                self.stdout.write("in percent: "+str(count_markings/count_units))
+                if type=="method" or type=="classmethod" or type=="staticmethod" or type=="function":
+                    methods[0]+=count_markings
+                    methods[1]+=count_units
+                elif type=="attribute" or type=="data":
+                    fields[0]+=count_markings
+                    fields[1]+=count_units
+                elif type=="section":
+                    modules[0]+=count_markings
+                    modules[1]+=count_units
+                elif type=="class" or type=="exception":
+                    classes[0]+=count_markings
+                    classes[1]+=count_units
+                elif type=="describe":
+                    describe[0]+=count_markings
+                    describe[1]+=count_units
+                total[0]+=count_markings
+                total[1]+=count_units
+
+            self.stdout.write("methods: "+str(methods[0]/methods[1]))
+            self.stdout.write("fields: "+str(fields[0]/fields[1]))
+            self.stdout.write("modules: "+str(modules[0]/modules[1]))
+            self.stdout.write("classes: "+str(classes[0]/classes[1]))
+            self.stdout.write("describe: "+str(describe[0]/describe[1]))
+
+
+        self.stdout.write("in total: "+str(total[0]/total[1]))
+
+
+
